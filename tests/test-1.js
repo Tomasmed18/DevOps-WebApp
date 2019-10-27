91b6583a573a43ab615a635b2e1ab1c51ceed15d
tomasmed18 <tomy-medina@hotmail.com>
2019-10-27 01:40:43 -0300

    Added 2 more http status code tests

--- tests/test-1.js
+++ tests/test-1.js
@@ -2,10 +2,24 @@ var expect  = require('chai').expect;
 var request = require('request');
 let assert = require('assert');
 
-describe('test http', function () {
+describe('http status code tests', function () {
 
   it('mainpage should respond without error',function(done){
     request.get('http://localhost:3000/', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('page with all albums should respond without error',function(done){
+    request.get('http://localhost:3000/albums', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('album creation page should respond without error',function(done){
+    request.get('http://localhost:3000/createAlbumMenu', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
