975090f9d605e9ea4b621852104298c91734684b
Tomasmed18 <tomy-medina@hotmail.com>
2019-10-29 17:23:05 -0300

    Added browser tests with selenium-webdriver and mocha

---
+++ tests/browser-tests.js
@@ -0,0 +1,28 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
+const IP_ADDRESS = "localhost";
+
+
+describe('Browser tests', () => {
+    const driver = new Builder().forBrowser('chrome').build();
+
+    it('should perform a search by album name', async () => {
+        await driver.get(`http://${IP_ADDRESS}:3000/`);
+        //await driver.sleep(2000);
+        await driver.findElement(By.id('bandName')).sendKeys('metall', Key.ENTER);
+        const title = await driver.getTitle();
+
+        expect(title).to.equal('Searching albums');
+    });
+
+    it('should perform a search by album name', async () => {
+        await driver.get(`http://${IP_ADDRESS}:3000/`);
+        //await driver.sleep(2000);
+        await driver.findElement(By.id('albumName')).sendKeys('kill', Key.ENTER);
+        const title = await driver.getTitle();
+
+        expect(title).to.equal('Searching albums');
+    });
+
+    after(async () => driver.quit());
+});
