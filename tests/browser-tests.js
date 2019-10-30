ff90a8cc9c00a4eeb245a2126dd6a84566407cef
Tomasmed18 <tomy-medina@hotmail.com>
2019-10-30 17:27:30 -0300

    Added chromedriver to browser-tests

--- tests/browser-tests.js
+++ tests/browser-tests.js
@@ -1,12 +1,13 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { expect } = require('chai');
+require('chromedriver')
 const IP_ADDRESS = "localhost";
 
 
 describe('Browser tests', () => {
     const driver = new Builder().forBrowser('chrome').build();
 
-    it('should perform a search by album name', async () => {
+    it('should perform a search by band name', async () => {
         await driver.get(`http://${IP_ADDRESS}:3000/`);
         //await driver.sleep(2000);
         await driver.findElement(By.id('bandName')).sendKeys('metall', Key.ENTER);
