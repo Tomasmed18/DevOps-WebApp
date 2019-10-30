43a716ae000b8f28d095b418386b612b71ede864
Tomasmed18 <tomy-medina@hotmail.com>
2019-10-30 17:41:49 -0300

    Changed from chrome browser to firefox

--- tests/browser-tests.js
+++ tests/browser-tests.js
@@ -1,16 +1,16 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { expect } = require('chai');
-require('chromedriver')
 const IP_ADDRESS = "localhost";
 
 
 describe('Browser tests', () => {
-    const driver = new Builder().forBrowser('chrome').build();
+    const driver = new Builder().forBrowser('firefox').build();
 
     it('should perform a search by band name', async () => {
         await driver.get(`http://${IP_ADDRESS}:3000/`);
-        //await driver.sleep(2000);
+        await driver.sleep(2000);
         await driver.findElement(By.id('bandName')).sendKeys('metall', Key.ENTER);
+        await driver.sleep(2000);
         const title = await driver.getTitle();
 
         expect(title).to.equal('Searching albums');
@@ -18,8 +18,9 @@ describe('Browser tests', () => {
 
     it('should perform a search by album name', async () => {
         await driver.get(`http://${IP_ADDRESS}:3000/`);
-        //await driver.sleep(2000);
+        await driver.sleep(2000);
         await driver.findElement(By.id('albumName')).sendKeys('kill', Key.ENTER);
+        await driver.sleep(2000);
         const title = await driver.getTitle();
 
         expect(title).to.equal('Searching albums');
